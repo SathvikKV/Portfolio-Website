578d387c50f1abd871167d3c23bd5c4c34fdfcb9
SathvikKV <156881388+SathvikKV@users.noreply.github.com>
2025-05-02 18:12:53 -0400

    Update data-dashboard-preview.tsx

--- new/components/data-dashboard-preview.tsx
+++ new/components/data-dashboard-preview.tsx
@@ -14,7 +14,7 @@ export default function DataDashboardPreview() {
       category: "analysis",
       description:
         "Implemented DCGAN and Conditional GAN using TensorFlow to generate synthetic MNIST and Fashion-MNIST images for data augmentation and class balancing.",
-      image: "/img/data/gan.png",
+      image: "/img/data/gan.PNG",
       link: "https://github.com/SathvikKV/Generative-Adversarial-Networks-GANs-/blob/main/notebooks/002443505_GAN_Notebook.ipynb",
     },
 
